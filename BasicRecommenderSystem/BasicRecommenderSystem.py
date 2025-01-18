e901d70715e2417aca3514f574de4e64f7cd4424
RHam007 <hussars@gmail.com>
2025-01-17 20:24:31 -0800

    Add project files.

---
+++ BasicRecommenderSystem/BasicRecommenderSystem.py
@@ -0,0 +1,17 @@
+# Simple Content Based Recommendation System
+# - Using genres to weigh likeliness of being recommended using graph plotting (x,y)
+# - Cosine Similarity A*B / (||A|| *||B||)
+# - For more complex recommendations, matrix increases, but same process is used.
+
+import pandas
+
+ratings_dataframe = pandas.read_csv("ratings.csv")
+
+ratings_dataframe
+
+
+
+
+# Neighborhood Based Collaborative Recommender
+# - Uses similar likes/habits/etc...
+
